1318f1991119056a2ff62898a4c9b5060ff13f98
yamaday <yamadayyi2@gmail.com>
2025-02-24 21:51:19 +0900

    不要なuseTransitionのインポートを削除

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import { useState, useTransition } from "react";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
